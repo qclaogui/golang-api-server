b5d336b5a34710381fe3518b9be75550ef3b77fe
qclaogui <qclaogui@gmail.com>
2019-05-30 13:23:22 +0800

    update demo

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"runtime"
 
 	"github.com/qclaogui/golang-api-server/version"
 
@@ -12,10 +13,17 @@ import (
 
 var port = "5012"
 
+var sourceLink = "https://github.com/qclaogui/golang-api-server"
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Info("request from: " + r.RemoteAddr)
+	var ver = fmt.Sprintf("Build on %s [commit: %s, build time: %s, release: %s]", runtime.Version(),
+		version.Commit, version.BuildTime, version.Release)
+
 	var name, _ = os.Hostname()
-	fmt.Fprintf(w, "Happy Coding! v1\nThis request was processed by host: %s", name)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<br/><center><h1>Happy Coding </h1><br/><code>%s</code><p><a href=%q target=_blank>source code</a></p></center><hr><br/>"+
+		"<center>this request was processed by host: %s</center>", ver, sourceLink, name)
 }
 
 func main() {
